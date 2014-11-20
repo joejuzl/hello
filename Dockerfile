f852b00a09431d3c3ce808c289538c9292230304
Juzl, Joseph <Joseph.Juzl@bskyb.com>
2014-11-20 11:39:01 +0000

    can't remember

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node
 
 ADD ./ /src
 
+WORKDIR /src
+
+RUN npm install
+
 EXPOSE 8000
 
 ENTRYPOINT ["node", "/src/app.js"]
--- app.js
+++ app.js
@@ -1,32 +1,16 @@
-var http = require('http');
+var express = require('express');
+var request = require('request');
+var app = express();
+
 
 var address = process.env.BACK_PORT;
 
-var options = {
-  host: address,
-  path: '/'
-};
 
-
-
-var server = http.createServer(function(request, response) {
-  http.request(options, function(resp) {
-    var str = '';
-    resp.on('data', function(chunk) {
-      str += chunk;
-    });
-    resp.on('end', function() {
-      response.writeHead(200, {
-        "Content-Type": "text/plain"
-      });
-      response.end(str);
-    });
-  }).end();
-
+app.get('/', function(req,res) {
+	request(address, function (error, response, body) {
+		res.send(body);
+	});
 });
 
-
-server.listen(8000);
-
-
-console.log("Server running at http://127.0.0.1:8000/");
+//app.listen(process.env.PORT || 4730);
+app.listen(8000);
