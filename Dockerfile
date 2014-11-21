02ae7d41b9fd17fdf4529670c49546a3a49c7b69
Juzl, Joseph <Joseph.Juzl@bskyb.com>
2014-11-21 14:38:51 +0000

    now the front proxy

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,4 @@ WORKDIR /src
 
 RUN npm install
 
-EXPOSE 8000
-
 ENTRYPOINT ["node", "/src/app.js"]
--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-var express = require('express');
-var request = require('request');
-var app = express();
+var http = require('http'),
+  httpProxy = require('http-proxy');
 
+var address = 'http://' + process.env.FRONTSERVICE_SERVICE_HOST + ':' + process.env.FRONTSERVICE_SERVICE_PORT;
 
-var address = "http://"+process.env.BACKSERVICE_SERVICE_HOST+":"+process.env.BACKSERVICE_SERVICE_PORT;
-
-
-app.get('/', function(req, res) {
-  request(address, function(error, response, body) {
-    res.type('text/plain');
-    res.send(body);
-  });
-});
-
-app.listen(process.env.PORT || 8000);
+httpProxy.createProxyServer({
+  target: address
+}).listen(8000);
